8dc2513e7ea94e1cce1112222671b561f72b3bc0
Vitor Santos <sr.santos235@gmail.com>
2021-08-19 01:46:14 -0300

    criação de mais js e alterações de html

--- jogo humanos/components.js
+++ jogo humanos/components.js
@@ -44,19 +44,19 @@
 
 
 function resposta() {
-    var respost = prompt("O que o grupo deve fazer? Escolha entre 1 ou 2.")
+    var respost = prompt("O que você deve fazer? Escolha entre 1 ou 2.")
     
     while (respost != 1 && respost != 2){
         alert ("Escolha inválida \n Clique novamente para responder")
-        respost = prompt("O que o grupo deve fazer?")
+        respost = prompt("O que você deve fazer?")
        
     }
 
     if (respost == 1) {
-        location.href = "vanyar2.html"
+        location.href = "../jogo humanos/jackR1.html"
        
     } else {
-        location.href = "gameover.html"
+        location.href = "../jogo humanos/jackR2.html"
   
     } 
     return 
