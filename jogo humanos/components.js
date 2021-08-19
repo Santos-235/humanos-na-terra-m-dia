8b3711dc1927ab185f244a18ad3d7040661ac07e
Vitor Santos <sr.santos235@gmail.com>
2021-08-18 21:07:02 -0300

    segunda e terceira criação de html

--- jogo humanos/components.js
+++ jogo humanos/components.js
@@ -1,44 +1,64 @@
-function jackresposta() {
+// function jackresposta() {
 
 
-    var respost = prompt("Como Jack começa sua defesa?")
-    var resposta = parseInt(respost)
+//     var respost = prompt("Como Jack começa sua defesa?")
+//     var resposta = parseInt(respost)
 
-    while (resposta != 1 && resposta != 2) {
+//     while (resposta != 1 && resposta != 2) {
 
 
-        alert("Resposta inválida! \n pressione, defender-se novamente")
-        // respost = "Como Jack começa sua defesa?"
-        // prompt(`${respost}`)
+//         alert("Resposta inválida! \n pressione, defender-se novamente")
+//         // respost = "Como Jack começa sua defesa?"
+//         // prompt(`${respost}`)
         
 
-        break
+//         break
 
 
 
-        //prompt("Como Jack começa sua defesa?")
-    }
+//         //prompt("Como Jack começa sua defesa?")
+//     }
     
 
-    if (resposta === 1) {
+//     if (resposta === 1) {
 
 
-        location.assign("jackR1.2.html")
+//         location.assign("jackR1.2.html")
 
+//     }
+//     else if (resposta === 2) {
+
+//         location.assign("jackR1.1.html")
+
+//     }
+//     else {
+//     }
+
+
+
+//     return resposta
+
+// }
+
+
+
+
+function resposta() {
+    var respost = prompt("O que o grupo deve fazer? Escolha entre 1 ou 2.")
+    
+    while (respost != 1 && respost != 2){
+        alert ("Escolha inválida \n Clique novamente para responder")
+        respost = prompt("O que o grupo deve fazer?")
+       
     }
-    else if (resposta === 2) {
 
-        location.assign("jackR1.1.html")
-
-    }
-    else {
-    }
-
-
-
-    return resposta
-
+    if (respost == 1) {
+        location.href = "vanyar2.html"
+       
+    } else {
+        location.href = "gameover.html"
+  
+    } 
+    return 
 }
-
-
 
